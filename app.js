34180057b8bb331ad2f7e272ee66d371883dbf71
legendhimslef <sabdulrahman971@gmail.com>
2021-06-02 03:11:00 +0530

    first push

---
+++ app.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { multiply, add } = require("./functions/calc");
+const { prefix, token } = require("./config.json");
+client.on("ready", async () => {
+  console.log("The bot is online");
+});
+
+client.on("message", async (message) => {
+  if (message.content.toLowerCase().startsWith(`${prefix}cal`)) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+
+    if (args[0].toLowerCase() === "multiply") {
+      args.shift();
+
+      return message.channel.send(multiply(args));
+    }
+    if (args[0].toLowerCase() === "add") {
+      args.shift();
+      add(args);
+      return message.channel.send(add(args));
+    }
+    // return message.channel.send(ans);
+  }
+});
+client.login(token);
---
+++ functions/calc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  multiply: (...x) => {
+    let args = x[0];
+    let ans = 1;
+    for (let i = 0; i < args.length; ) {
+      const arg = args[i];
+      ans = ans * arg;
+      i++;
+      if (i === args.length) {
+        return ans;
+      }
+    }
+  },
+  add: (...x) => {
+    let args = x[0];
+    let ans = 0;
+    for (let i = 0; i < args.length; ) {
+      try {
+        if (args[i].split("").includes(".")) {
+          var arg = parseFloat(args[i]);
+          console.log(arg);
+        } else {
+          arg = parseInt(args[i]);
+          console.log(arg, "else");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      ans = ans + arg;
+      i++;
+      if (i === args.length) {
+        return ans;
+      }
+    }
+  },
+};
